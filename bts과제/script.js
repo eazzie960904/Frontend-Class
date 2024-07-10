956f5a71e7ecc3167bd3c98a399cafe6dcc5e162
eazzie960904 <eazzie960904@gmail.com>
2024-07-10 14:44:03 +0900

    robloc

---
+++ bts과제/script.js
@@ -0,0 +1,33 @@
+const items = document.querySelectorAll(".photos li");
+
+items.forEach((item) => {
+  item.addEventListener("mouseover", () => {
+    item.style.transform = "translateY(-20px)";
+    item.style.transition = "all 0.3s";
+  });
+  item.addEventListener("mouseout", () => {
+    item.style.transform = "translateY(0)";
+  });
+});
+
+const gnbLi = document.querySelectorAll(".topNav > li");
+gnbLi.forEach((li) => {
+  li.addEventListener("mouseover", () => {
+    const lnb = li.querySelector(".lnb");
+    const aTag = li.querySelector("a");
+    if (lnb) {
+      lnb.style.maxHeight = lnb.scrollHeight + "px";
+      lnb.style.opacity = "1";
+      aTag.classList.add("active");
+    }
+  });
+  li.addEventListener("mouseout", () => {
+    const lnb = li.querySelector(".lnb");
+    const aTag = li.querySelector("a");
+    if (lnb) {
+      lnb.style.maxHeight = "0";
+      lnb.style.opacity = "0";
+      aTag.classList.remove("active");
+    }
+  });
+});
