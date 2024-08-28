efffea3db94eda0a13068d55b70aa62e9d881285
eazzie960904 <eazzie960904@gmail.com>
2024-08-28 23:44:38 +0900

    240828

---
+++ script_home/240826/01js_weatherAPI/env.js
@@ -0,0 +1,3 @@
+const API_KEY="faa55b6b68a3cb6cc3f485b72b230aa1";
+
+export default{API_KEY};
---
+++ script_home/240828/script1.js
@@ -0,0 +1,7 @@
+const canvas = document.querySelector("canvas");
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+const ctx = canvas.getContext("2d");
+
