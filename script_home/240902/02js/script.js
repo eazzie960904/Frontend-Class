79a14889ac111e3f790af70b4d3d19988a949167
eazzie960904 <eazzie960904@gmail.com>
2024-09-02 23:41:10 +0900

    240902

---
+++ script_home/240902/02js/script.js
@@ -0,0 +1,10 @@
+anime({
+    targets: "li",
+    translateX: 250,
+    easing:"linear",
+    loop: true,
+    direction: "alternate",
+
+    delay: anime.stagger(100),
+    endDelay: anime.stagger(100),
+});
