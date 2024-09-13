a3d4fb5637fc490a96df3a8cdfa495e9db5a0ca7
eazzie960904 <eazzie960904@gmail.com>
2024-09-13 09:38:03 +0900

    240913

---
+++ reactproject/list/src/App.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import './App.css';
+import TodoBoard from './components/TodoBoard';
+
+function App() {
+  
+  return (
+    <main>
+     
+    </main>
+  );
+}
+
+export default App;
