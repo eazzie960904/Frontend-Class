3b144fda4bc9f18b0b391e55f7c711505e7b2936
eazzie960904 <eazzie960904@gmail.com>
2024-09-24 17:47:06 +0900

    240924

--- header/src/components/Header.js
+++ header/src/components/Header.js
@@ -1,19 +1,30 @@
 import React from "react";
-import './Header.css';
+import "./Header.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-function Header(){
-    return(
-        <main className="header">
-            <div className="kakaoLogo"></div>
-            <input type="text" id="searchBarHeader" placeholder="검색"/>
-            <button id="addStoryHeader">오늘의 스토리를 들려주세요.</button>
-            <div className="leftIconHeader">
-            <button><i class="fa-solid fa-user-group"></i></button>
-            <button><i class="fa-regular fa-bell"></i></button>
-            <button><i class="fa-regular fa-circle-user"></i></button>
-            </div>
-        </main>
-    )
+function Header() {
+  const kakaoLogo = styled.div`
+    width: 240px;
+    height: 80px;
+    background: url("./images/kakaoLogo.png");
+  `;
+  return (
+    <main className="header">
+      <div className="kakaoLogo"></div>
+      <input type="text" id="searchBarHeader" placeholder="검색" />
+      <button id="addStoryHeader">오늘의 스토리를 들려주세요.</button>
+      <div className="leftIconHeader">
+        <button>
+          <i class="fa-solid fa-user-group"></i>
+        </button>
+        <button>
+          <i class="fa-regular fa-bell"></i>
+        </button>
+        <button>
+          <i class="fa-regular fa-circle-user"></i>
+        </button>
+      </div>
+    </main>
+  );
 }
 export default Header;
