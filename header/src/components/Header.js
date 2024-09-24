0f9e98bba1d2bdd123e6b25d10d13834f87692ef
eazzie <eazzie960904@gmail.com>
2024-09-25 08:22:43 +0900

    240925

--- header/src/components/Header.js
+++ header/src/components/Header.js
@@ -1,17 +1,28 @@
 import React from "react";
 import "./Header.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styled from "styled-components";
+import image from "./images/kakaoLogo.png";
+
+const KakaoLogo = styled.div`
+  width: 240px;
+  height: 80px;
+  background: url(${image}) center/cover;
+`;
+
+const SearchBarHeader = styled.input`
+  width: 240px;
+  height: 44px;
+  border: none;
+  border-radius: 24px;
+  background: #fbfbfb;
+`;
 
 function Header() {
-  const kakaoLogo = styled.div`
-    width: 240px;
-    height: 80px;
-    background: url("./images/kakaoLogo.png");
-  `;
   return (
     <main className="header">
-      <div className="kakaoLogo"></div>
-      <input type="text" id="searchBarHeader" placeholder="검색" />
+      <KakaoLogo />
+      <SearchBarHeader type="text" id="searchBarHeader" placeholder="검색" />
       <button id="addStoryHeader">오늘의 스토리를 들려주세요.</button>
       <div className="leftIconHeader">
         <button>
