9744e583279f0dc210c8344ff1b315c211f04175
eazzie960904 <eazzie960904@gmail.com>
2024-09-25 18:39:55 +0900

    240925

--- mainPage/src/App.js
+++ mainPage/src/App.js
@@ -1,10 +1,11 @@
+import React from "react";
 import "./App.css";
-import Header from "./components/Header";
+import MainPage from "./components/MainPage";
 
 function App() {
   return (
     <div>
-      <Header />
+      <MainPage />
     </div>
   );
 }
